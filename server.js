a54fee12bb71d2411671f0e4a32f64c73cdfb72b
jusbuckingham <jus.buckingham@gmail.com>
2021-12-29 09:33:05 -0800

    adding routes

--- server.js
+++ server.js
@@ -13,6 +13,26 @@ app.get("/actors", async (request, response) => {
     }
 });
 
+app.get("/movies", async (request, response) => {
+
+    try {
+        const moviesArray = await Movies.find({});
+        response.json({ moviesArray });
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+app.get("/tvshows", async (request, response) => {
+
+    try {
+        const tvshowsArray = await Tvshows.find({});
+        response.json({ atvshowsArray });
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
 app.listen(3000, () => {
     console.log("Server is running at port 3000")
 });
