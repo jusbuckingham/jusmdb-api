2dfa8251d1f90ac80eff8114cd8068e244f0c404
jusbuckingham <jus.buckingham@gmail.com>
2021-12-28 21:27:36 -0800

    adding playground js

---
+++ playground.js
@@ -0,0 +1,14 @@
+// const { Actors } = require("./models");
+
+// Actors.insertMany({
+//     url: "",
+//     title: "",
+//     image_url: "",
+//     synopsis: ""
+//   })
+//   .then((newActors) => {
+//       console.log(newActors);
+//   })
+//   .catch((error) => {
+//       console.log(error);
+// })
