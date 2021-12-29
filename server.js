23e8ae814b471b65053f0387a372841181f3d3ee
jusbuckingham <jus.buckingham@gmail.com>
2021-12-28 20:35:27 -0800

    adding server.js and other adjustments to models

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const { Actors } = require("./models");
+const app = express();
+
+app.use(express.json());
+app.get("/actors", async (request, response) => {
+
+    try {
+        const actorsArray = await Season.find({});
+        response.json({ actorsArray });
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+app.listen(3000, () => {
+    console.log("Server is running at port 3000")
+});
